957d2f9d8c76169d89b14b04cb4914bce8d55355
Dzintars <dzintars2@gmail.com>
2023-08-22 22:14:00 +0300

    Added questions and question screen

---
+++ lib/answer_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(this.answerText, this.onTap, {super.key});
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color.fromARGB(255, 117, 40, 133),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 40,
+        ),
+      ),
+      child: Text(answerText),
+    );
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class Questionscreen extends StatefulWidget {
   const Questionscreen({super.key});
@@ -21,10 +22,25 @@ class Questionscreen extends StatefulWidget {
 class _QuestionsScreenState extends State<Questionscreen> {
   @override
   Widget build(context) {
-    return const Center(
-      child: Column(mainAxisSize: MainAxisSize.min, children: [
-        Text('test'),
-      ]),
+    return SizedBox(
+      width: double.infinity,
+      height: double.infinity,
+      child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'The question...',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 35),
+            AnswerButton('Answer #1', () {}),
+            AnswerButton('Answer #2', () {}),
+            AnswerButton('Answer #3', () {}),
+            AnswerButton('Answer #4', () {}),
+          ]),
     );
   }
 }
